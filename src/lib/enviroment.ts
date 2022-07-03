b6daf660d33cfdcfd8f010689691e7b8b32a3fd8
Plutoy <99151858+PlutoyDev@users.noreply.github.com>
2022-07-03 13:31:22 +0800

    create enviroment file

---
+++ src/lib/enviroment.ts
@@ -0,0 +1,23 @@
+import { config } from 'dotenv';
+
+config();
+
+export const NODE_ENV = process.env.NODE_ENV ?? 'development';
+export const PORT = process.env.PORT ?? 2000;
+
+export const DISCORD_APP_ID = process.env.DISCORD_APP_ID ?? '990032179604099124';
+export const DISCORD_PUBLIC_KEY =
+  process.env.DISCORD_PUBLIC_KEY ?? '4fb2e58f3bd89aea1be7497a4a76223c6e82af47c26e926df27158236543c416';
+export const DISCORD_OAUTH_SECRET = process.env.DISCORD_OAUTH_SECRET;
+export const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+
+const requiredEnvs = {
+  DISCORD_OAUTH_SECRET,
+  DISCORD_BOT_TOKEN,
+};
+
+Object.entries(requiredEnvs).forEach(([key, value]) => {
+  if (value == undefined || value == '') {
+    throw new Error(`${key} is not defined`);
+  }
+});
