a39757dc4f8d45eac77c6bddf82f096a0bb33b99
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2022-07-24 14:55:47 +0800

    Merge Feature/default template (#4)
    * create default template seeder
    
    * Added Channel selection and default templating
    
    * change in template syntax
    
    * move seed script
    
    * Schema changes
    
    * Script for deleting webhooks

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,4 +1,6 @@
-import { SlashCommand, SlashCreator, CommandContext } from 'slash-create';
+import { APIGuildChannel, APIWebhook, ChannelType, GuildChannelType, Routes } from 'discord-api-types/v10';
+import { CommandContext, ComponentType, SlashCommand, SlashCreator } from 'slash-create';
+import { DiscordRest } from '../lib/discordRest';
 import { prisma } from '../lib/prisma';
 
 export class InitCommand extends SlashCommand {
@@ -19,7 +21,9 @@ export class InitCommand extends SlashCommand {
     const guild = await prisma.guild.findFirst({ where: { id: guildId } });
     if (guild) {
       ctx.editOriginal({
-        content: `This server (${guildId}) had been already initialized.`,
+        content: `This server (${guildId}) had been already initialized.
+To change the the timestamp channel use \`/config timestamp channel\`
+\`/help\` for commands list`,
       });
     } else {
       await prisma.guild.create({
@@ -27,8 +31,97 @@ export class InitCommand extends SlashCommand {
           id: guildId,
         },
       });
-      ctx.editOriginal({
+      const channels = (await DiscordRest.get(Routes.guildChannels(guildId))) as APIGuildChannel<GuildChannelType>[];
+      const textChannelsSelect = channels
+        .filter(c => c.type === ChannelType.GuildText || c.type === ChannelType.GuildNews)
+        .map(c => ({ value: c.id, label: c.name })) as { value: string; label: string }[];
+
+      await ctx.editOriginal({
         content: `This server (${guildId}) is now initialized.`,
+      });
+
+      await ctx.sendFollowUp({
+        content: 'Please select a channel to use for the timestamp:',
+        components: [
+          {
+            type: ComponentType.ACTION_ROW,
+            components: [
+              {
+                type: ComponentType.SELECT,
+                custom_id: `${guildId}-timestamp-channel`,
+                options: textChannelsSelect,
+              },
+            ],
+          },
+        ],
+      });
+
+      ctx.registerComponent(`${guildId}-timestamp-channel`, async cctx => {
+        const channelId = cctx.values[0];
+        if (channelId) {
+          const { id: webhookID, token: webhookToken } = (await DiscordRest.post(Routes.channelWebhooks(channelId), {
+            reason: `@${ctx.user.username} has initialized the bot`,
+            body: { name: 'Timestampy' },
+          })) as Required<APIWebhook>;
+
+          const webhook = await prisma.webhook.create({
+            data: {
+              id: webhookID,
+              token: webhookToken,
+              channelId: channelId,
+              Guild: {
+                connect: {
+                  id: guildId,
+                },
+              },
+              TimestampConfig: {
+                connectOrCreate: {
+                  where: {
+                    id: guildId,
+                  },
+                  create: {
+                    Guild: {
+                      connect: {
+                        id: guildId,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            include: {
+              TimestampConfig: true,
+            },
+          });
+
+          const configId = webhook.TimestampConfig?.id;
+
+          await ctx.send('Timestamp channel updated');
+
+          if (!configId) {
+            return ctx.send('Something went wrong, please try again');
+          }
+
+          const defaultTmpl = await prisma.template.findMany({
+            where: {
+              configId: '@default',
+            },
+            select: {
+              recordKey: true,
+              template: true,
+            },
+          });
+
+          if (defaultTmpl) {
+            await prisma.template.createMany({
+              skipDuplicates: true,
+              data: defaultTmpl.map(t => ({ configId, ...t })),
+            });
+
+            await ctx.sendFollowUp('Default template has been applied.\nTo change it use `/config timestamp template`');
+          }
+        }
+        ctx.unregisterComponent(`${guildId}-timestamp-channel`);
       });
     }
   }
---
+++ src/scripts/db/seed.ts
@@ -0,0 +1,136 @@
+import { RecordType } from '../../../prisma/build';
+import prisma from '../../lib/prisma';
+
+const clear = process.argv[2] === 'delete';
+
+const seedRecords = async () => {
+  if (clear) {
+    await prisma.record.deleteMany();
+  }
+  await prisma.record.createMany({
+    skipDuplicates: true,
+    data: [
+      {
+        name: 'Sanctuary Geyser Wax',
+        key: 'recur_geyser_sanctuary',
+        type: RecordType.Recur,
+        interval: 120,
+        offset: 0,
+        duration: 15,
+        collectibleAfter: 5,
+      },
+      {
+        name: 'Forest Grandma Dinner Wax',
+        key: 'recur_dinner_forest',
+        type: RecordType.Recur,
+        interval: 120,
+        offset: 30,
+        duration: 15,
+        collectibleAfter: 5,
+      },
+      {
+        name: 'Sanctuary Turtle Wax',
+        key: 'recur_turtle_sanctuary',
+        type: RecordType.Recur,
+        interval: 120,
+        offset: 50,
+        duration: 10,
+      },
+      {
+        name: 'Shattering Shards',
+        key: 'recur_shards_shattering',
+        type: RecordType.Recur,
+        interval: 120,
+        offset: 58,
+        duration: 50,
+        collectibleAfter: 10,
+      },
+      {
+        name: 'Days of Rainbow 2022',
+        key: 'event_2022_rainbow_days',
+        type: RecordType.Event,
+        start: new Date(2022, 5, 30),
+        end: new Date(2022, 6, 14),
+      },
+      {
+        name: 'Season of Shattering',
+        key: 'event_shattering_season',
+        type: RecordType.Event,
+        start: new Date(2022, 6, 11),
+        end: new Date(2022, 8, 26),
+      },
+    ],
+  });
+};
+
+const seedTimestampConfig = async () => {
+  if (clear) {
+    await prisma.timestampConfig.deleteMany();
+  }
+  await prisma.timestampConfig.createMany({
+    skipDuplicates: true,
+    data: [
+      {
+        id: '@default',
+        guildId: '@default',
+      },
+    ],
+  });
+};
+
+const seedTemplate = async () => {
+  if (clear) {
+    await prisma.template.deleteMany();
+  }
+  seedTimestampConfig();
+  await prisma.template.createMany({
+    skipDuplicates: true,
+    data: [
+      {
+        configId: '@default',
+        recordKey: 'main',
+        template: `**__Main Game__** \${now}
+Daily Reset: \${daily_reset_next} (\${daily_reset_next,R})
+Eden Reset: \${eden_reset_next} (\${eden_reset_next,R})
+
+**__Traveling Spirit \${traveling_spirit_count}__**
+Arrival: \${traveling_spirit_start} (\${traveling_spirit_start,R})
+Departure: \${traveling_spirit_end} (\${traveling_spirit_end,R})`,
+      },
+      {
+        configId: '@default',
+        recordKey: 'recur_geyser_sanctuary',
+        template: `**__Sanctuary Geyser Wax__**
+\${occurrences,t,➡️}
+Next: \${next} {\${next,R}}
+\${ongoing_until, Ongoing Until: % (%R)}`,
+      },
+      {
+        configId: '@default',
+        recordKey: 'recur_dinner_forest',
+        template: `**__Forest Grandma Dinner Wax__**
+\${occurrences,t,➡️}
+Next: \${next} {\${next,R}}
+\${ongoing_until, Ongoing Until: % (%R)}`,
+      },
+      {
+        configId: '@default',
+        recordKey: 'recur_turtle_sanctuary',
+        template: `**__Sanctuary Turtle Wax__**
+\${occurrences,t,➡️}
+Next: \${next} {\${next,R}}
+\${ongoing_until, Ongoing Until: % (%R)}`,
+      },
+      {
+        configId: '@default',
+        recordKey: 'recur_shards_shattering',
+        template: `**__Shattering Shards__**
+\${occurrences,t,➡️}
+Next: \${next} {\${next,R}}
+\${ongoing_until, Ongoing Until: % (%R)}`,
+      },
+    ],
+  });
+};
+
+seedRecords().then(() => console.log('Records seeded'));
