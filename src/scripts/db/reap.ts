d1022030ba15150e0e84614667260e48133de51f
PlutoyDev <99151858+PlutoyDev@users.noreply.github.com>
2022-07-24 14:57:37 +0800

    remove useless code

--- src/scripts/db/reap.ts
+++ src/scripts/db/reap.ts
@@ -2,10 +2,8 @@ import { Routes } from 'discord-api-types/v10';
 import { DiscordRest } from '../../lib/discordRest';
 import prisma from '../../lib/prisma';
 
-const discard = process.argv[2] === 'delete';
-
 const reapWebhooks = async () => {
-  const msgs = await prisma.message.deleteMany();
+  await prisma.message.deleteMany();
   const webhooks = await prisma.webhook.findMany();
 
   await Promise.all(webhooks.map(async webhook => DiscordRest.delete(Routes.webhook(webhook.id))));
@@ -14,5 +12,3 @@ const reapWebhooks = async () => {
 };
 
 reapWebhooks();
-
-const reapFromGuild = async () => {};
